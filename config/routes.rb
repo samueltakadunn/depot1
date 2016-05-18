2832b52813fcbfd3a77e0a03a253d395002d0bfa
samueltakadunn <sdunn95@sbcglobal.net>
2016-05-18 22:30:15 +0000

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
-Rails.application.routes.draw do
+Depot::Application.routes.draw do
+  resources :line_items
+  resources :carts
+  get "store/index"
   resources :products
-  resources :products
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
+  # ...
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -41,7 +46,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,18 +1,13 @@
 require 'test_helper'
-
-class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-  }
-end
-
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:       19.95
+    }
   end
 
   test "should get index" do
@@ -28,11 +23,13 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
   end
+
+  # ...
 
   test "should show product" do
     get :show, id: @product
@@ -45,9 +42,11 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
+
+  # ...
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
